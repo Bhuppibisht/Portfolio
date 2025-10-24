b13fa69c49e851fb940c0e37f437e5314de51cad
Bhuppibisht <bhupendrasingh05b@gmail.com>
2025-10-24 10:23:55 +0530

    fix

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,44 +1,44 @@
-import { sendEmail } from "@/lib/sendMail";
+// import { sendEmail } from "@/lib/sendMail";
 
 
-export async function POST(req: Request) {
-    try {
-        const { name, email, msg } = await req.json();
+// export async function POST(req: Request) {
+//     try {
+//         const { name, email, msg } = await req.json();
 
-        if (!name || !email || !msg) {
-            return Response.json({
-                success: false,
-                message: "All fields are required"
-            })
-        }   
+//         if (!name || !email || !msg) {
+//             return Response.json({
+//                 success: false,
+//                 message: "All fields are required"
+//             })
+//         }   
         
-        const res = await sendEmail({ name, email, msg })
+//         const res = await sendEmail({ name, email, msg })
 
-        if (res.success) {
-            console.log(res.message);
-        }
-        else{
-            return Response.json({
-                success:false,
-                message:"Something went wrong"
-            })
-        }
-        return Response.json({
-            success: true,
-            message:"Email sent successfully"
-        })
+//         if (res.success) {
+//             console.log(res.message);
+//         }
+//         else{
+//             return Response.json({
+//                 success:false,
+//                 message:"Something went wrong"
+//             })
+//         }
+//         return Response.json({
+//             success: true,
+//             message:"Email sent successfully"
+//         })
 
 
 
-    } catch (error) {
-        console.error("Error sending mail", error);
-        return Response.json({
-            success: false,
-            message: "Error sending mail"
-        },
-            {
-                status: 500
-            }
-        )
-    }
-}
+//     } catch (error) {
+//         console.error("Error sending mail", error);
+//         return Response.json({
+//             success: false,
+//             message: "Error sending mail"
+//         },
+//             {
+//                 status: 500
+//             }
+//         )
+//     }
+// }
