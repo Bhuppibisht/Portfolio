36922086f98327a1ef2f4be0d81805241e2fc26b
Bhuppibisht <bhupendrasingh05b@gmail.com>
2025-10-24 11:04:49 +0530

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export function Navbar() {
 
     const handleDownloadResume = () => {
         
-        const resumeUrl = "https://docs.google.com/document/d/1aTVTYnN9v-DBiIGcAtzP0Lzj_Cuzk7ph/edit?usp=drive_link&ouid=100101563820177392474&rtpof=true&sd=true"
+        const resumeUrl = "https://docs.google.com/document/d/1aTVTYnN9v-DBiIGcAtzP0Lzj_Cuzk7ph/edit?usp=sharing&ouid=100101563820177392474&rtpof=true&sd=true"
         const link = document.createElement("a")
         link.href = resumeUrl
         link.download = "Bhupendra_Singh_Resume.pdf"
